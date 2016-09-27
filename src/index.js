3f12822632d033658a1df9d4e21cabb58f7c3e28
Thomas Ghysels <info@thomasg.be>
2016-09-27 21:34:49 +0200

    Add logging

--- src/index.js
+++ src/index.js
@@ -12,10 +12,22 @@ export default function livereload (options = { watch: '' }) {
   const server = lr.createServer(options)
   server.watch(resolve(process.cwd(), options.watch))
 
+  var enabled = false
+
   return {
     name: 'livereload',
     banner () {
       return `document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':${port}/livereload.js?snipver=1"></' + 'script>');`
+    },
+    ongenerate () {
+      if (!enabled) {
+        enabled = false
+        console.log(green('LiveReload enabled'))
+      }
     }
   }
+}
+
+function green (text) {
+  return '\u001b[1m\u001b[32m' + text + '\u001b[39m\u001b[22m'
 }
