cb07b758bd6412e2f451613cdebfe67bee9fb85b
Yohan Gomez <yohan.gz@hotmail.com>
2019-01-24 00:08:32 +0530

    Rollup 1.x.x support & update deprecated dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { createServer } from 'livereload'
-import { resolve } from 'path'
+import { createServer } from 'livereload';
+import { resolve } from 'path';
 
 export default function livereload (options = { watch: '' }) {
   if (typeof options === 'string') {
@@ -10,9 +10,9 @@ export default function livereload (options = { watch: '' }) {
     options.watch = options.watch || '';
   }
 
-  var enabled = options.verbose === false
-  const port = options.port || 35729
-  const server = createServer(options)
+  var enabled = options.verbose === false;
+  const port = options.port || 35729;
+  const server = createServer(options);
 
   // Start watching
   if (Array.isArray(options.watch)) {
@@ -21,32 +21,32 @@ export default function livereload (options = { watch: '' }) {
     server.watch(resolve(process.cwd(), options.watch))
   }
 
-  closeServerOnTermination(server)
+  closeServerOnTermination(server);
 
   return {
     name: 'livereload',
-    banner () {
+    banner() {
       return `(function(l, i, v, e) { v = l.createElement(i); v.async = 1; v.src = '//' + (location.host || 'localhost').split(':')[0] + ':${port}/livereload.js?snipver=1'; e = l.getElementsByTagName(i)[0]; e.parentNode.insertBefore(v, e)})(document, 'script');`
     },
-    ongenerate () {
+    generateBundle() {
       if (!enabled) {
-        enabled = true
-        console.log(green('LiveReload enabled'))
+        enabled = true;
+        console.log(green('LiveReload enabled'));
       }
     }
   }
 }
 
 function green (text) {
-  return '\u001b[1m\u001b[32m' + text + '\u001b[39m\u001b[22m'
+  return '\u001b[1m\u001b[32m' + text + '\u001b[39m\u001b[22m';
 }
 
 function closeServerOnTermination (server) {
-    const terminationSignals = ['SIGINT', 'SIGTERM']
+    const terminationSignals = ['SIGINT', 'SIGTERM'];
     terminationSignals.forEach((signal) => {
         process.on(signal, () => {
-            server.close()
-            process.exit()
+            server.close();
+            process.exit();
         })
     })
 }
