86703df1100fad581f8c2dd353f326145bb31c71
rixo <rixo@rixo.fr>
2020-01-22 22:33:27 +0100

    remove term signals handling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { createServer } from 'livereload'
 import { resolve } from 'path'
 
 let server
-let removeTerminationListeners
 
 export default function livereload(options = { watch: '' }) {
   if (typeof options === 'string') {
@@ -13,21 +12,17 @@ export default function livereload(options = { watch: '' }) {
     options.watch = options.watch || ''
   }
 
-  // release previous server instance if rollup is reloading configuration in watch mode
+  // release previous server instance if rollup is reloading configuration
+  // in watch mode
   if (server) {
     server.close()
     server = null
-  }
-  // we need to release previous listeners, or livereload will try to close
-  // already closed servers on termination
-  if (removeTerminationListeners) {
-    removeTerminationListeners()
-    removeTerminationListeners = null
   }
 
   let enabled = options.verbose === false
   const port = options.port || 35729
   const snippetSrc = options.clientUrl ? JSON.stringify(options.clientUrl) : `'//' + (window.location.host || 'localhost').split(':')[0] + ':${port}/livereload.js?snipver=1'`
+
   server = createServer(options)
 
   // Start watching
@@ -36,8 +31,6 @@ export default function livereload(options = { watch: '' }) {
   } else {
     server.watch(resolve(process.cwd(), options.watch))
   }
-
-  removeTerminationListeners = closeServerOnTermination(server);
 
   return {
     name: 'livereload',
@@ -55,25 +48,4 @@ export default function livereload(options = { watch: '' }) {
 
 function green (text) {
   return '\u001b[1m\u001b[32m' + text + '\u001b[39m\u001b[22m'
-}
-
-function closeServerOnTermination (server) {
-  const terminationSignals = ['SIGINT', 'SIGTERM']
-
-  const listener = () => {
-    server.close()
-    process.exit()
-  }
-
-  const cleanup = () => {
-    terminationSignals.forEach(signal => {
-      process.removeListener(signal, listener)
-    })
-  }
-
-  terminationSignals.forEach(signal => {
-    process.on(signal, listener)
-  })
-
-  return cleanup
 }
